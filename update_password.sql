1a0572e18ddc1ac8a044f2da8e6280364d1ad4e1
Jose Raphael Ernesto Perez Hernandez <rafa_rmk@yahoo.es>
2025-10-28 13:57:36 -0600

    Limpieza: Eliminar archivos backup, test y carpetas antiguas MVC

--- update_password.sql
+++
@@ -1,4 +0,0 @@
-﻿UPDATE usuarios 
-SET password = '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi' 
-WHERE id = 5;
-SELECT id, nombre, email, LEFT(password, 20) as password_inicio FROM usuarios WHERE id = 5;
